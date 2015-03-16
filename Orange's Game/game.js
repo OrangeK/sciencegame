1878926b214918f4349024684719e3c60d6b4758
Orange Karpusi <OrangeK@users.noreply.github.com>
2015-03-16 16:30:13 -0700

    changed option stuff
    moved some stuff and changed some stuff

--- Orange's Game/game.js
+++ Orange's Game/game.js
@@ -1,8 +1,8 @@
 var startgame = function(){
  confirm("GO!");
- confirm("When inputting a prompt answer, please put the verb that best shows what is going on.");
- var option1 = prompt("You enter the dungeon. You have been searching for the location of the dungeon for many years now, and have finally found it. In your hand you carry a single torch. On your belt, there is a pickaxe and a shovel, along with a rather lengthy coil of rope. You know there is a treasure hidden within the dungeon. You can either TAKE A LOOK AROUND, START DIGGING, or TRY TO DISARM SOME OF THE TRAPS that could be hidden around.").toLowerCase();
+ confirm("When inputting a prompt answer, please put the capitalized word.”);
+ var option1 = prompt("You enter the dungeon. You have been searching for the location of the dungeon for many years now, and have finally found it. In your hand you carry a single torch. On your belt, there is a pickaxe and a shovel, along with a rather lengthy coil of rope. You know there is a treasure hidden within the dungeon. You can either take a LOOK around, start DIGging, or try to DISARM some of the traps that could be hidden around.").toLowerCase();
  switch(option1){
   case 'look":
-   var option1-1 = prompt("You take a look around. You see some paintings on the walls
+   confirm(“You take a look around. You see some paintings on the walls that depict the gruesome deaths of explorers at the hands of what looks like a giant duck. You doubt that that is what actually killed all those people. Ducks aren’t killers. You also notice that some of the floor tiles appear to have little lines running down the middle”)
 }
