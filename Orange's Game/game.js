5e59ca7fc6097657097a1581c425688da758bd21
Orange Karpusi <OrangeK@users.noreply.github.com>
2015-03-19 17:20:52 -0700

    moar stuff added

--- Orange's Game/game.js
+++ Orange's Game/game.js
@@ -6,7 +6,17 @@ var startgame = function(){
   case 'look':
    confirm("You cast your torchlight on the area that surrounds you, and take a look around. You see some paintings on the walls that depict the gruesome deaths of explorers at the hands of what looks like a giant duck. You doubt that that is what actually killed all those people. Ducks aren’t killers. You also notice that some of the floor tiles appear to have little lines running down the middle");
    var option1 = prompt("With this knowledge in mind, you can try to NAVIGATE down to where the treasure should be, or EXPLORE and try to find out more about this dungeon.").toLowerCase();
-  break;
+   switch(option1){
+    case 'navigate':
+     confirm("While walking around, you step on a hidden tile, and fall into a pit of spikes. Oops.");
+    break;
+    case 'explore':
+     confirm("While looking at the walls, you accidentally but a hidden button, and a boulder falls down from above and crushes you. Oops.");
+    break;
+    default:
+      confirm("I'm sorry, but your input could not be understood. please try again.");
+    };
+   break;
   case 'dig':
    confirm("You begin to dig. After digging through a few layers, you drop down into a dimly lit corridor, and your shovel dents on the hard stone floor. It is now useless, but you don't throw it away just in case.")
    var option2 = prompt("You decide that you should probably try to find a different way to get to the treasure. After some examination, you spot some faded dark red marks on the wall. You can either FOLLOW the markings, or FIND your own way to the treasure.").toLowerCase();
@@ -17,7 +27,11 @@ var startgame = function(){
     case 'find':
      confirm("You know the general location of the treasure, so you head in that direction.");
      confirm("After a bit of walking, you come upon a staircase. It leads to the general area of where you need to go, so you go down it. When you come out, the first thing you notice is the heat. It is very warm. You also notice the floor is made of dark stone cubes instead of tan tiles.")
-     
+     confirm("You walk a bit farther, and find the treasure! You win!");
+     confirm("Now, the game would not usually be this short, but due to time constraints, it is. Sorry about that.");
+   break;
+   default:
+    confirm("I'm sorry, but your input could not be understood. please try again.");
    };
   break;
   case 'disarm':
@@ -26,7 +40,7 @@ var startgame = function(){
    switch(option3){
     case 'pickaxe':
      confirm("You pull out your pickaxe, and poke the inside of the hole with the smaller end. You immediately pull it back out, right before a poison dart shoots out and just misses the top of your head. You are very glad that it missed.");
-     var option3s1 = prompt("").toLowerCase();
+     //var option3s1 = prompt("").toLowerCase();
     break;
      case 'finger':
      confirm("You stick your finger in the hole. It gets stuck. A poison dart shoots itself into your finger. The poison sets in quickly, and you die soon after.");
